c326ea9ef6b104c62ace499cf126fb09ab55ac91
Avraj19 <avrajsbw@gmail.com>
2020-05-18 17:09:11 +0100

    monday class 18-05-2020

--- README_18_05.md
+++ README_18_05.md
@@ -2,7 +2,36 @@
 
 Intro in to Python
  
-- Supperoted on mutiple systems and platforms 
-- Objecct-Oriented Programming (OOPs) driven
+- Supported on multiple systems and platforms 
+- Object-Oriented Programming (OOPs) driven
 - Helps to improve programs Productivity
-- 
+
+## Variables 
+
+What is a variables?
+A variable are like boxes, they can store strings, numbers and more ...
+
+Why are variables used? 
+Variables are use to reduce the repeated code
+
+```python
+variable_name = 'variable_type'
+```
+
+Naming conventions for variables:
+- All lower case
+- Use underscore to separate words 
+
+There are 3 main types of variable types that can be used:
+- 'string'
+- INT, this a number
+- Boolean, this is True or False 
+
+You can ask the user to input data and save it in a variable
+```python
+first_name = input('What is your name?')
+print(first_name)
+
+```
+
+
---
+++ Week 3/Variables.py
@@ -0,0 +1,45 @@
+# Simple print
+# print('Hello World')
+
+
+# name = 'Avraj'
+# print(name)
+
+
+# print(type(name))
+
+# age = 1
+# name = 'new born'
+# name_age = name + age
+#
+# print(name_age)
+
+# favorite_color = 'Blue'
+# best_team = 'Not Arsenal'
+# team_color = favorite_color + best_team
+# print(type(team_color))
+
+# You can ask the user to input data and save it in a variable
+# first_name = input('What is your name?')
+# print(first_name)
+
+# cust_name = input('What is your full name? \n')
+# cust_age = int(input('What is your age? \n'))
+# cust_dob = input('What is your date of birth? \n')
+# print(type(cust_name, cust_dob, cust_age))
+
+# # who is older age or new_age
+# age = 23
+# new_age = 24.6
+# # In this case we used the greater than symbol
+# # This is a boolean and it will return a true or false
+# print(new_age > age)
+#
+# # Another way is using a if statement
+# # This will print out which ever number is greater
+# if age < new_age:
+#     print(age)
+# else:
+#     print(new_age)
+
+
